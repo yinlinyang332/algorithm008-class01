c38bc351a2eacd3f465760460c973040f7ce5cb7
YinQin <yinlinyang332@gmail.com>
2020-04-18 14:36:12 +0800

    climbStairs

--- Week_01/practice/climbStairs.js
+++ Week_01/practice/climbStairs.js
@@ -21,13 +21,32 @@ var climbStairs0 = function (n) {
 	return (memory[n] = climbStairs(n - 1) + climbStairs(n - 2))
 }
 
-var climbStairs = function (n) {
+var climbStairs1 = function (n) {
 	var memory = [1, 2]
 	var i = 2
-	while ( n >= i) {
-		memory[i] = memory[i - 1] + memory[ i -2 ]
+	while (n >= i) {
+		memory[i] = memory[i - 1] + memory[i - 2]
 		i++
 	}
 
 	return memory[n - 1]
+}
+
+var climbStairs = function (n) {
+	if (n < 2) return n
+
+	var f1 = 1
+	var f2 = 2
+	var f3 = 3
+
+	var i = 3
+	while ( i < n + 1) {
+		f3 = f1 + f2
+		f1 = f2
+		f2 = f3
+
+		i++
+	}
+
+	return f3
 }
